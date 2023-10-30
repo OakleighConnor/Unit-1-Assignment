f024d3086b6c6c3c5ef1a2ae2ea52174ffac83f3
OakleighConnor <146819272+OakleighConnor@users.noreply.github.com>
2023-10-30 12:10:25 +0000

    Initial commit

---
+++ Assets/Scripts/HelperScript.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelperScript : MonoBehaviour
+{
+    LayerMask groundLayerMask;
+    // Start is called before the first frame update
+    void Start()
+    {
+        groundLayerMask = LayerMask.GetMask("Ground");
+    }
+    public void FlipObject(bool flip)
+    {
+        // get the SpriteRenderer component
+        SpriteRenderer sr = gameObject.GetComponent<SpriteRenderer>();
+
+        //Flip the sprite calling the method
+
+        if (flip == true)
+        {
+            sr.flipX = true;
+        }
+        else
+        {
+            sr.flipX = false;
+        }
+    }
+    public bool GroundCheck(bool isGrounded)
+    {
+        //Raycasting
+        Color hitColor = Color.blue;
+        float laserLength = 10f;
+        //Makes 3 lines directly beneath the player to register the floor
+        Vector3 rayOffset = new Vector3(0, -0.21f, 0);
+        RaycastHit2D hit = Physics2D.Raycast(transform.position + rayOffset, Vector2.down, laserLength, groundLayerMask);
+
+        //If any of the lines are touching the ground then the player can jump.
+        if (hit.collider != null)
+        {
+            hitColor = Color.red;
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+        Debug.DrawRay(transform.position + rayOffset, Vector2.down * laserLength, hitColor);
+        return isGrounded;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public bool isGrounded = false;
+    float speed = 5f;
+    HelperScript helper;
+    SpriteRenderer spi;
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        spi = GetComponent<SpriteRenderer>();
+        helper = gameObject.AddComponent<HelperScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {
+        //Jump
+        if ((Input.GetKeyDown("w") == true) && (helper.GroundCheck(isGrounded) == true))
+        {
+            rb.AddForce(new Vector3(0, 8, 0), ForceMode2D.Impulse);
+        }
+
+        //Grounded Movement
+        if (Input.GetKey("a") == true)
+        {
+            transform.position = new Vector2(transform.position.x + (-speed * Time.deltaTime), transform.position.y);
+        }
+        if (Input.GetKey("d") == true)
+        {
+            transform.position = new Vector2(transform.position.x + (speed * Time.deltaTime), transform.position.y);
+        }
+    }
+}
